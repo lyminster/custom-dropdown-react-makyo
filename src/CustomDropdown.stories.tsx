0008ef6edafe78bcd80a908d2f2f81783f3a013a
Rommy <rommygautanto@gmail.com>
2024-07-02 11:23:18 +0700

    fix:remove uneceserry import

--- src/CustomDropdown.stories.tsx
+++ src/CustomDropdown.stories.tsx
@@ -1,5 +1,3 @@
-// src/CustomDropdown.stories.tsx
-import React from "react";
 import { Meta, StoryFn } from "@storybook/react";
 import CustomDropdown, { CustomDropdownProps } from "./CustomDropdown";
 
